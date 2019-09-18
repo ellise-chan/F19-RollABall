4e4bc90c2f712d871624d5f33d9e33f3159a62e8
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-09-18 17:27:04 -0400

    Include Count AND WIN

--- Assets/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
     private int count;
@@ -13,6 +16,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -31,6 +36,16 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = " You Win!!";
         }
     }
 }

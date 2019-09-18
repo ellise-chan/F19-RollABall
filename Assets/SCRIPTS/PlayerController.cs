9d3fe6886869ef5461661ac9a77de669ab8e5cc7
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-09-18 15:22:39 -0400

    CameraController

--- Assets/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
     private Rigidbody rb;
 
     void Start()
@@ -17,7 +19,7 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
         
     }
 }
